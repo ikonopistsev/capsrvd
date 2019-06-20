58a3ee85ee4439fc5b1219385d098216fb677060
meil <14835841+ikonopistsev@users.noreply.github.com>
2019-06-20 11:16:19 +0300

    fix }

--- server/controller.js
+++ server/controller.js
@@ -95,14 +95,4 @@ receive() {
     }
 }
 
-// const dynamic_sort = (property) => {
-//     const sortOrder = 1;
-//     if(property[0] === "-") {
-//         sortOrder = -1;
-//         property = property.substr(1);
-//     }
-//     return (a, b) => {
-//         const result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
-//         return result * sortOrder;
-//     }
-// }
+}
